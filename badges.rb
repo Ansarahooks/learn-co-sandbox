7a133949e5c898993cd4d3c6a3857c559d3ed8d0
ansara.hooks <ansara.hooks@gmail.com>
2018-11-14 22:45:38 +0000

    automatically backed up by learn

---
+++ badges.rb
@@ -0,0 +1,20 @@
+# badge_maker
+ def badge_maker (name)
+   puts "Hello, my name is #{name}."
+ end
+ badge_maker
+
+# batch_badge_creator
+def batch_badge_creator (names,index)
+  puts "Hello, my name is #{names}."
+end
+batch_badge_creator (names[])
+
+# assign_rooms
+def assign_rooms (speakers,index)
+   puts "Hello, #{speakers[index]}! You'll be assigned to room #{index}!"
+ end
+ assign_rooms
+ 
+ # printer
+ def printer
---
+++ hello_world.rb
@@ -0,0 +1,6 @@
+names = ["Rob", "Emily", "Dean", "Tammy", "Brit","Ansara"]
+
+names.each do |name|
+  puts "Hi, #{name}"
+end
+
